52aefe1366a8bcab9d72011766c676f7547abf8d
Kalpesh Shah <43423119+KalpeshSShah@users.noreply.github.com>
2020-10-26 16:17:20 -0400

    update

--- app/src/main/java/Github/App.java
+++ app/src/main/java/Github/App.java
@@ -5,7 +5,7 @@ package Github;
 
 public class App {
     public String getGreeting() {
-        return "Hello real World!";
+        return "Hello World!";
     }
 
     public static void main(String[] args) {
